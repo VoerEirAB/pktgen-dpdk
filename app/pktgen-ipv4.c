370d328a03b1e800e0b53a7cca87878117ab0a95
Safiyat Reza <safiyat@voereir.com>
2017-04-20 16:25:24 +0530

    Finishes #144011767. Increased default TTL from 4 to 64. Same as MoonGen.

--- app/pktgen-ipv4.c
+++ app/pktgen-ipv4.c
@@ -97,7 +97,7 @@ pktgen_ipv4_ctor(pkt_seq_t *pkt, ipHdr_t *ip)
 	ip->vl              = (IPv4_VERSION << 4) | (sizeof(ipHdr_t) / 4);
 
 	ip->tlen            = htons(tlen);
-	ip->ttl             = 4;
+	ip->ttl             = 64;
 	ip->tos             = 0;
 
 	pktgen.ident        += 27;	/* bump by a prime number */
